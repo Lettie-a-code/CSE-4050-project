fdfbb8f9d18402dcda8b923a4a9b43bee53df128
Elizabeth Warren <008720420@coyote.csusb.edu>
2025-10-20 19:25:07 -0700

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ShoppingCartAPI.Models;
+
+namespace ShoppingCartAPI.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+        //Nullable or initialized properties avoid "non-nullable must contain a value" warning
+        public DbSet<Product> Products { get; set; }
+        public DbSet<CustomerProfile> CustomerProfiles { get; set; }
+    }
+}
+
+
+
+
+
